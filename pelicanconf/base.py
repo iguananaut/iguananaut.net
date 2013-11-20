916918449b62461e2d02d6536eae69d8b1a1b799
Erik M. Bray <erik.m.bray+iguananaut@gmail.com>
2013-11-20 16:39:04 -0500

    Some more basic site configuration

--- pelicanconf/base.py
+++ pelicanconf/base.py
@@ -2,31 +2,41 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+import codecs
 import os
+from os.path import join
 
-AUTHOR = u'Erik Bray'
-SITENAME = u'Iguananaut'
-SITEURL = ''
+AUTHOR = 'Erik M. Bray'
+SITENAME = 'Iguananaut.net'
+SITESUBTITLE = 'Trying not to be too pretentious here'
+SITE_DESCRIPTION = 'Personal blog of Erik M. Bray'
+SITEURL = 'http://iguananaut.net'
 
 TIMEZONE = 'US/East'
 
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = 'en'
+
+
+# Path settings
+PATH = join(os.pardir, 'content')
+OUTPUT_PATH = join(os.pardir, 'output')
+
+
+# Theme settings
+THEME = 'pelican-elegant'
+CUSTOM_CSS = True
+RECENT_ARTICLES_COUNT = 5
+LANDING_PAGE_ABOUT = {
+    'title': SITESUBTITLE,
+    'details': codecs.open(join('templates', 'about.html'),
+                           encoding='utf8').read()
+}
+SITE_LICENSE = codecs.open(join('templates', 'license.html'),
+                           encoding='utf8').read()
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-
-OUTPUT_PATH = os.path.join(os.pardir, 'output')
-
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
